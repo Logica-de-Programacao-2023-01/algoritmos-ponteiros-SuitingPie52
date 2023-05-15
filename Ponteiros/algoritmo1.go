387b537217478d0cdcc18151b467e5597a11bf5e
Gabriel Antonio Navarro Paiva <gabriel.npaiva@dinfor.uniceub>
2023-05-15 09:32:00 -0300

    resolução

--- Ponteiros/algoritmo1.go
+++ Ponteiros/algoritmo1.go
@@ -1,1 +1,30 @@
-package Ponteiros
+package main
+
+import "fmt"
+
+// Escreva uma função que receba um ponteiro para um inteiro e um valor inteiro ``n``.
+// A função deve atualizar o valor do inteiro para a soma dos ``n`` primeiros números naturais.
+
+func somaNaturais(p *int, n int) {
+
+	soma := 0
+
+	for i := 1; i <= n; i++ {
+
+		soma += i
+
+	}
+
+	*p = soma
+
+}
+
+func main() {
+
+	x := 0
+
+	somaNaturais(&x, 5)
+
+	fmt.Println(x)
+
+}
